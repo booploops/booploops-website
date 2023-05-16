2275a186f6e02a554600cf10ea0a503802087428
booploops <49113086+booploops@users.noreply.github.com>
2023-05-16 02:56:59 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>booploops</h1>
+	<hr>
+	You may not like it, but this is what PERFECTION looks like.
+</section>
+
+<style>
+
+</style>
